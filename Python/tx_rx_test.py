63d7d1c456aa172b999ffc85f657641cc4a4f32e
Chase Timmins <chase.timmins@gmail.com>
2020-05-27 22:15:43 -0700

    Update tx_rx_test.py

--- Python/tx_rx_test.py
+++ Python/tx_rx_test.py
@@ -6,5 +6,9 @@ from SDR_Setup import *
 if __name__ == '__main__':
 	sdr = pluto_setup(True, 1e8,True,True,1e8)
 
+	# Set Sample rate and carrier frequency for the QAM (TX/RX)_LO already set
 	fs = sdr.sample_rate
 	fc = fs / 100
+
+	# Create bit sequency to be transmitted
+	
